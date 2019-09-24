abbfcb034a4d81d52e83fbc853408e76d31dfb0d
Antranik Medina <anmedina@syr.edu>
2019-09-24 11:51:44 -0400

    Just Debugging now

--- blackjack.java
+++ blackjack.java
@@ -1,17 +1,27 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class blackjack{
-
-
     public static void main(String[] args) {
         
-
-        //get player info
+        //get player info and make a player to compare to the original player
         player playerOne = makePlayer();
-        
-        System.out.println(playerOne.getName() + " " + playerOne.getAmount());
 
+        //Shows initial values
+        System.out.println("------------------------------------------------------");
+        System.out.println("Player: " + playerOne.getName() + "\nPlaying amount: " + playerOne.getAmount());
+        System.out.println("------------------------------------------------------");
 
+        //How much the player started with
+        double initialAmount = playerOne.getAmount();
+
+        //Play blackjack
+        playBlackjack(playerOne);
+
+        //End game, shows ending values 
+        System.out.println("------------------------------------------------------");
+        System.out.println(playerOne.getName() + ", you finished with " + playerOne.getAmount() + "!");
+        System.out.println("Your profit: " + (playerOne.getAmount()-initialAmount));
+        System.out.println("------------------------------------------------------");
 
         return;
     }
@@ -19,21 +29,336 @@ public class blackjack{
     //Make player function taking in the name and the ammount they wanna play with
     public static player makePlayer(){
         String name;
-        int amount;
+        double amount;
 
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
         System.out.println("Enter your name");
         name = myObj.nextLine();
 
         System.out.println("How much money do you plan on playing with?");
-        amount = myObj.nextInt();
+        amount = myObj.nextDouble();
 
         player playerOne = new player(name, amount);
 
         return(playerOne);
     }
 
+    //Game of black jack, the function runs into the user has no money left or chooses to leave
+    public static void playBlackjack(player user){
+        String keepPlaying = "Yes";
 
+        //While the User has more than 0 dollars to play with, the game keeps going
+        while(user.getAmount() > 0){
+            user = blackjackRound(user);
+
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Do you wish to keep playing? (Yes/No)");
+            keepPlaying = scan.nextLine();
+            
+            //Asks if the the user wants to keep playing or not, if not it breaks the while loop
+            if(keepPlaying.compareTo("No")==0)
+                break;
+        }
+
+        return;
+    }
+
+    //A single round of poker
+    //This program is gonna work a bit differently than a normal casino
+    //The deck of cards will be "shuffled" after every round and you can't double down nor split
+    //Everything else still stands
+    public static player blackjackRound(player user){
+        //Amount user is going to bet
+        int betAmount;
+
+        //Choice of player if he/she would like to push or stand
+        String playerChoice;
+
+        //variable if player wants to keep standing or hitting
+        boolean choice = true;
+
+        //Hand totals int array
+        int[] handTotals = new int[2];
+        handTotals[0] = 0;
+        handTotals[1] = 0;
+
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("How much would you like to bet for this round?:");
+        betAmount = myObj.nextInt();
+
+        //Function to make a new deck every round
+        ArrayList<card> deck = makeDeck();
+
+        //Create player cards and dealers cards
+        ArrayList<card> playerCards = new ArrayList<card>(15);
+        ArrayList<card> dealerCards = new ArrayList<card>(15);
+        
+        //Go every other like a casino
+        playerCards.add(deck.get(0));
+        dealerCards.add(deck.get(1));
+        playerCards.add(deck.get(2));
+        dealerCards.add(deck.get(3));
+
+        //counts how many cards are used
+        int cardCounter = 4;
+
+
+        //User rounds of picking cards
+        while(choice == true){
+            handTotals = readHandBeforeFlop(playerCards, dealerCards);
+
+            //Player busts
+            if(handTotals[0]>21){
+                user.setAmount(user.getAmount() - betAmount);
+                System.out.println("You busted!");
+                System.out.println("You lost " + betAmount);
+                System.out.println("You know have a total of " + user.getAmount() + "left to play with");
+                choice = false;
+            }
+            if(handTotals[0]==21){
+                System.out.println("Blackjack!");
+                //User gets 1.5 times the bet amount for blackjack
+                user.setAmount(user.getAmount() + (1.5 * betAmount));
+                System.out.println("You know have a total of " + user.getAmount() + "left to play with"); 
+                choice = false;
+            }
+            else{ //If player chooses to push
+                System.out.println("Would you like to push or stand? (Type Push/Stand");
+                playerChoice = myObj.nextLine();
+                if(playerChoice.compareTo("Push")==0){
+                    playerCards.add(deck.get(cardCounter));
+                    cardCounter++;
+                }
+                else{ //If player chooses to stand
+                    choice = false;
+                }
+            }
+        }
+
+        //If player has more than 21 than end round
+        if(handTotals[0] > 21){
+            return(user);
+        }
+        else{
+            handTotals = readHandAfterFlop(playerCards, dealerCards);
+            while(handTotals[1]<17){
+                if(handTotals[1]>21){//Dealer busts
+                    System.out.println("Dealer busted! You win!");
+                    user.setAmount(user.getAmount() + betAmount);
+                    System.out.println("You know have a total of " + user.getAmount() + "left to play with");     
+                }
+                else if(handTotals[1]==21 || handTotals[1]>handTotals[0]){//Dealer gets blackjack or wins outright
+                    System.out.println("Dealer Wins!");
+                    user.setAmount(user.getAmount() - betAmount);
+                    System.out.println("You lost " + betAmount);
+                    System.out.println("You know have a total of " + user.getAmount() + "left to play with");
+                }
+                handTotals = readHandAfterFlop(playerCards, dealerCards);
+            }
+            //If dealer has more than 17 on the flop
+            if(handTotals[1]<handTotals[0]){//You have more
+                System.out.println("You win!");
+                user.setAmount(user.getAmount() + betAmount);
+                System.out.println("You know have a total of " + user.getAmount() + "left to play with"); 
+            }
+            else if(handTotals[0]<handTotals[1]){//Dealer has more
+                System.out.println("Dealer Wins!");
+                user.setAmount(user.getAmount() - betAmount);
+                System.out.println("You lost " + betAmount);
+                System.out.println("You know have a total of " + user.getAmount() + "left to play with");
+            }
+            else{
+                System.out.println("It's a push! No one wins!");
+            }
+        }
+
+        return(user);
+    }
+
+    //make the deck of card to play with
+    public static ArrayList<card> makeDeck(){
+        ArrayList<card> deck = new ArrayList<card>(50);
+
+        //Create 50 cards
+        //13 different cards
+        //4 different suits
+        for(int i=0; i<50; i++){
+            int value = (int) (Math.random() * ((1 - 13) + 1)) + 1;
+            int suit = (int) (Math.random() * ((4 - 1) + 1)) + 1;
+            card deckCard = new card(value, suit);
+            deck.add(deckCard);
+        }
+
+        return(deck);
+    }
+
+    //switch statment that takes in a card and returns the numerical value of the card
+    public static int cardToNumber(card cardVal){
+        int answer = 0;
+        if(cardVal.value > 10){
+            answer = 10;
+        }
+        else if(cardVal.value==1)
+            answer = 11;
+        else{
+            answer = cardVal.value;
+        }
+
+        return(answer);
+    }
+
+    //Returns the string the name of the card
+    public static String cardValReader(card cardVal){
+        String cardPlayed = new String();
+        
+        //Textual value of card
+        switch(cardVal.value){
+            case 1:
+                cardPlayed = "Ace of ";
+                break;
+            case 2:
+                cardPlayed = "2 of ";
+                break;
+            case 3:
+                cardPlayed = "3 of ";
+                break;
+            case 4:
+                cardPlayed = "4 of ";
+                break;
+            case 5:
+                cardPlayed = "5 of ";
+                break;
+            case 6:
+                cardPlayed = "6 of ";
+                break;
+            case 7:
+                cardPlayed = "7 of ";
+                break;
+            case 8:
+                cardPlayed = "8 of ";
+                break;
+            case 9:
+                cardPlayed = "9 of ";
+                break;
+            case 10:
+                cardPlayed = "10 of ";
+                break;
+            case 11:
+                cardPlayed = "J of ";
+                break;
+            case 12:
+                cardPlayed = "Q of ";
+                break;
+            case 13:
+                cardPlayed = "K of ";
+                break;    
+        }
+
+        //Textual value of the suit of the card
+        switch(cardVal.suit){
+            case 1:
+                cardPlayed.concat("Spades");
+                break;
+            case 2:
+                cardPlayed.concat("Clubs");
+                break;
+            case 3:
+                cardPlayed.concat("Hearts");
+                break;
+            case 4:
+                cardPlayed.concat("Diamonds");
+                break;
+        }
+
+        return(cardPlayed);
+    }
+
+    //After dealer flop; Reading out the hands and totals function
+    public static int[] readHandAfterFlop(ArrayList<card> playerHand, ArrayList<card> dealerHand){
+        int[] handTotals = new int[2];
+        boolean aceFind = false;
+
+        //Player hands and total
+        System.out.println("------------------------------------------------------");
+        System.out.print("You have:");
+        try{
+            for(int i = 0; i < playerHand.size(); i++){
+                System.out.print(" " + cardValReader(playerHand.get(i)));
+                handTotals[0] = handTotals[0] + cardToNumber(playerHand.get(i));
+            }
+        }
+        catch(NullPointerException e){
+            System.out.println("oops");
+        }
+        System.out.println(" totaling: " + handTotals[0]);
+
+        //Dealer hands and total
+        System.out.print("Dealer has:");
+        try{
+            for(int i = 0; i < playerHand.size(); i++){
+                System.out.print(" " + cardValReader(playerHand.get(i)));
+                handTotals[1] = handTotals[1] + cardToNumber(playerHand.get(i));
+            }
+        }
+        catch(NullPointerException e){}
+        //If dealer has more than 21 and has an ace, then subtract 10 from total
+        aceFind = findAce(dealerHand);
+        if(handTotals[1]>21 && aceFind==true)
+            handTotals[1] = handTotals[1] - 10;
+        System.out.println(" totaling: " + handTotals[1]);
+        System.out.println("------------------------------------------------------");
+
+        return(handTotals);
+    }
+
+
+    //Pre dealer flop; reading out the hands and totals function
+    public static int[] readHandBeforeFlop(ArrayList<card> playerHand, ArrayList<card> dealerHand){
+        int[] handTotals = new int[2];
+        handTotals[0] = 0;
+        handTotals[1] = 0;
+
+        //Tries to find an ace in the hand
+        boolean aceFind = false;
+
+        //Player hands and total
+        System.out.println("------------------------------------------------------");
+        System.out.print("You have:");
+        try{
+            for(int i = 0; i < playerHand.size(); i++){
+                System.out.print(" " + cardValReader(playerHand.get(i)));
+                handTotals[0] = handTotals[0] + cardToNumber(playerHand.get(i));
+            }
+        }
+        catch(NullPointerException e){}
+        //If player's hand is over 21 but has an ace, remove 10 from the total hand value
+        aceFind = findAce(playerHand);
+        if(handTotals[0]>21 && aceFind==true)
+            handTotals[0] = handTotals[0] - 10;
+        System.out.println(" totaling: " + handTotals[0]);
+
+        //Dealer hands and total but only showing the second card
+        System.out.print("Dealer is showing:" + cardValReader(playerHand.get(1)));
+        System.out.println("------------------------------------------------------");
+
+        return(handTotals);
+    }
+
+    //Function to find an ace in a hand
+    public static boolean findAce(ArrayList<card> hand){
+        boolean aceFind = false;
+        
+        try{
+            for(int i = 0; i < hand.size(); i++){
+                if(hand.get(i).value == 1)
+                    aceFind = true;
+            }
+        }
+        catch(NullPointerException e){}
+
+        return aceFind;
+        
+    }
 
 
 }
--- player.java
+++ player.java
@@ -1,17 +1,16 @@
 public class player{
     private String name;
-    private int amount;
+    private double amount;
 
     public player(){
         this.name = "User";
         this.amount = 100;
     }
 
-    public player(String name, int amount){
+    public player(String name, double amount){
         this.name = name;
         this.amount = amount;
     }
-
 
     public void setName(String name){
         this.name = name;
@@ -21,11 +20,11 @@ public class player{
         return name;
     }
 
-    public void setAmount(int amount){
+    public void setAmount(double amount){
         this.amount = amount;
     }
 
-    public int getAmount(){
+    public double getAmount(){
         return amount;
     }
 }
